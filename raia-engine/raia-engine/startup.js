73aea17090fe2970e2ba2d128c3d41e956ec0a4b
dolphilia <dolphilia@outlook.com>
2022-09-09 23:10:52 +0900

    スタートアップjavascriptの内容を変更

--- raia-engine/raia-engine/startup.js
+++ raia-engine/raia-engine/startup.js
@@ -5,9 +5,14 @@ var b = 200;
 
 print(a + b);
 
+function rnd(max) {
+  return Math.floor(Math.random() * max);
+}
+
 while(!glfw_window_should_close()) {
+    fill_rect_rgb(0,0,100,100, 255,255,0);
     for (var x = 0; x < 640; x++) {
-        set_pixel(x, 10);
+        set_pixel_rgb(x, 10, rnd(255),rnd(255),rnd(255));
     }
     glfw_redraw();
     glfw_pool_events();
