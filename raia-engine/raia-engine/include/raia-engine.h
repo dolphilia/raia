3044a751970624ed634f326252bba4973bd802fa
dolphilia <dolphilia@outlook.com>
2022-09-09 23:14:37 +0900

    Create raia-engine.h

---
+++ raia-engine/raia-engine/include/raia-engine.h
@@ -0,0 +1,28 @@
+//
+//  raia-engine.h
+//  raia-engine
+//
+//  Created by dolphilia on 2022/09/09.
+//
+
+#ifndef raia_engine_h
+#define raia_engine_h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "duktape.h"
+#include "platforms.h"
+#include "util_io.h"
+#include "util_string.h"
+#include "util_pixel.h"
+#include "util_math.h"
+#include "wrapper_glfw.h"
+#include "wrapper_duktape.h"
+#include "static_header.h"
+#include "static_pixel_data.h"
+#include "static_window.h"
+#include "raia_gl.h"
+#include "raia_al.h"
+#include "raia_duktape.h"
+
+#endif /* raia_engine_h */
