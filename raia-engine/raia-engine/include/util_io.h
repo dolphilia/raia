fe2bad1470f26e172bbac557fdaf6b7d7c809d20
dolphilia <dolphilia@outlook.com>
2022-09-09 23:11:49 +0900

    環境に依存しないUtility関連のソースを追加

---
+++ raia-engine/raia-engine/include/util_io.h
@@ -0,0 +1,17 @@
+//
+//  util_io.h
+//  raia-engine
+//
+//  Created by dolphilia on 2022/09/09.
+//
+
+#ifndef util_io_h
+#define util_io_h
+
+#include "platforms.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+char* load_string_filename(const char* filename);
+
+#endif /* util_io_h */
