a26f00b3933fa4f96550f045a03d6e0b49618cb0
dolphilia <dolphilia@outlook.com>
2022-10-15 09:57:46 +0900

    Update static_header.h

--- src/static/static_header.h
+++ src/static/static_header.h
@@ -11,6 +11,18 @@
 #include <stdint.h>
 #include "util/util_string.h"
 
+typedef struct {
+    uint8_t red;
+    uint8_t green;
+    uint8_t blue;
+    uint8_t alpha;
+} _color_t;
+
+typedef struct {
+    uint32_t x;
+    uint32_t y;
+} _point_t;
+
 // raia_header ヘッダー構造体
 typedef struct raia_header_t {
     int32_t window_width; // ウィンドウの幅と高さ
@@ -19,12 +31,17 @@ typedef struct raia_header_t {
     int32_t resolution_height;
     int32_t magnification; // 倍率 = WINDOW_WIDTH / SCREEN_WIDTH
     int32_t samples_per_pixel; // 3=RGBカラー, 4=RGBAカラー
-    //uint8_t* pixel_data;
+    _color_t current_color; // 現在のカラーRGBA
+    _point_t current_point; // 現在のポイントX,Y
+    bool exist_update_callback;
     char window_title[512]; // ウィンドウの初期状態のタイトル
 } raia_header_t;
 
 void init_raia_header(void);
 raia_header_t get_raia_header(void);
 void set_resolution_size(int width, int height);
+void set_title(char* title);
+void set_current_color(int r, int g, int b);
+void set_exist_update_callback(bool is_exist);
 
 #endif /* raia_struct_h */
