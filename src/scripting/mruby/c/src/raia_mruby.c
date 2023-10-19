9dc2b4a0ec667ed5d921daa9157e80edcef4d1d6
dolphilia <dolphilia@outlook.com>
2023-10-19 11:27:00 +0900

    update

--- src/scripting/mruby/c/src/raia_mruby.c
+++ src/scripting/mruby/c/src/raia_mruby.c
@@ -1,40 +1,59 @@
 #include "raia_mruby.h"
 
-#include <stdio.h>
-
-#include "mruby.h"
-#include "mruby/compile.h"
-
+mrb_value raia_lib_open(mrb_state *mrb, mrb_value self) {
+    char *dll_file = NULL;
+    mrb_get_args(mrb, "z", &dll_file);
 #ifdef __WINDOWS__
-#define RAIA_EXPORT __declspec(dllexport)
-#else
-#define RAIA_EXPORT
+    const char *extension = "dll";
 #endif
+#ifdef __MACOS__
+    const char *extension = "dylib";
+#endif
+#ifdef __LINUX__
+    const char *extension = "so";
+#endif
+    char dll_file_extension[500];
+    SPRINTF(dll_file_extension, "%s.%s", dll_file, extension);
+    void *handle = add_plugin_hash(dll_file_extension);
+    return mrb_fixnum_value((int64_t)(uintptr_t)handle);
+}
 
-mrb_value my_c_function(mrb_state *mrb, mrb_value self) {
-    // 引数を取得します。この例では1つの文字列を取得します。
-    char *str = NULL;
-    mrb_get_args(mrb, "z", &str);
-
-    printf("Hello, %s\n", str);
-
-    // mruby関数は常に値を返さなければなりません。何も返さない場合はnilを返します。
+mrb_value raia_lib_add(mrb_state *mrb, mrb_value self) {
+    mrb_int handle_i;
+    char *dll_func_name = NULL;
+    mrb_get_args(mrb, "iz", &handle_i, dll_func_name);
+    void *handle = (void *)handle_i;
+    add_plugin_func_hash(handle, dll_func_name);
     return mrb_nil_value();
 }
 
-RAIA_EXPORT const char *init(int argc, char *argv[]) {
+RAIA_API const char *init(int argc, char *argv[]) {
     mrb_state *mrb = mrb_open();
     if (!mrb) { /* handle error */
-    }
-
-    const char *filename = "main.mrb";
-    FILE *file = fopen(filename, "r");
-    if(file == NULL) {
-        printf("%s file not open!\n", filename);
         return NULL;
     }
-    mrb_load_file(mrb, file);
-    fclose(file);
+    struct RClass *cls;
+    cls = mrb_define_class(mrb, "Lib", mrb->object_class);
+    mrb_define_class_method(mrb, cls, "open", raia_lib_open, MRB_ARGS_REQ(1));
+    mrb_define_class_method(mrb, cls, "add", raia_lib_add, MRB_ARGS_REQ(2));
+
+//    const char *filename = "main.mrb";
+//    FILE *file = fopen(filename, "r");
+//    if(file == NULL) {
+//        printf("%s file not open!\n", filename);
+//        return NULL;
+//    }
+//    mrb_load_file(mrb, file);
+//    fclose(file);
+
+    const char *mruby_code = "MyClass.c_function('Hello from mruby!')";
+    mrb_value result = mrb_load_string(mrb, mruby_code);
+    if (mrb->exc) {
+        mrb_print_error(mrb);
+        mrb_close(mrb);
+        return NULL;
+    }
+
 
     // mrb_load_string(mrb, str) to load from NULL terminated strings
     // mrb_load_nstring(mrb, str, len) for strings without null terminator or with known length
