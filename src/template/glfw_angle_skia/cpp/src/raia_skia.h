c7a709d8dbbd16eebb395699cff93bb89660d54e
dolphilia <dolphilia@outlook.com>
2023-12-14 14:21:37 +0900

    Rename raia_gui.h to raia_skia.h and remove unused code
    The file raia_gui.h was renamed to raia_skia.h to better reflect its purpose. Subsequently, all references to the old file name were updated. Additionally, large blocks of unused and commented-out code were removed, decluttering the file and improving readability.

---
+++ src/template/glfw_angle_skia/cpp/src/raia_skia.h
@@ -0,0 +1,24 @@
+//
+// Created by dolphilia on 2023/08/08.
+//
+
+#ifndef RAIA_GUI_RAIA_SKIA_H
+#define RAIA_GUI_RAIA_SKIA_H
+
+extern "C" {
+#include <time.h>
+#include "../../../../common/c/utility/export_api.h"
+#include "../../../../common/c/utility/platform.h"
+#include "../../../../common/c/wrapper/wrapper_yyjson.h"
+#define STB_IMAGE_IMPLEMENTATION
+#include "../../../../third_party/c/nothings/stb/stb_image.h"
+#include "GLFW/glfw3.h" // Will drag system OpenGL headers
+#include "EGL/egl.h"
+#include "GLES3/gl32.h"
+
+#ifdef __WINDOWS__
+#define GL_TEXTURE0 0x84C0
+#endif
+}
+
+#endif //RAIA_GUI_RAIA_SKIA_H
