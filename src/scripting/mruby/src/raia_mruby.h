7927abd16d0df48e11dc0fe6a665eda2ce0eef08
dolphilia <dolphilia@outlook.com>
2023-12-28 02:57:34 +0900

    Update relative paths in project configuration files
    Adjusted the relative paths to include files and directory mappings in some project configuration files like vcs.xml, raia_mruby.h, and CMakeLists.txt. This refactoring ensures accuracy in paths, reflecting the correct file structure and improving the maintainability of the code.

--- src/scripting/mruby/src/raia_mruby.h
+++ src/scripting/mruby/src/raia_mruby.h
@@ -4,13 +4,13 @@
 #include <stdio.h>
 #include "mruby.h"
 #include "mruby/compile.h"
-#include "../../../../common/c/utility/export_api.h"
-#include "../../../../common/c/utility/platform.h"
-#include "../../../../common/c/wrapper/wrapper_yyjson.h"
-#include "../../../../common/c/static/static_plugin_hash.h"
-#include "../../../../common/c/static/static_func_hash.h"
-#include "../../../../common/c/static/static_entrust.h"
-#include "../../../../common/c/utility/util_file.h"
+#include "../../../common/c/utility/export_api.h"
+#include "../../../common/c/utility/platform.h"
+#include "../../../common/c/wrapper/wrapper_yyjson.h"
+#include "../../../common/c/static/static_plugin_hash.h"
+#include "../../../common/c/static/static_func_hash.h"
+#include "../../../common/c/static/static_entrust.h"
+#include "../../../common/c/utility/util_file.h"
 
 #ifdef _WIN32
 #include <stdlib.h>
