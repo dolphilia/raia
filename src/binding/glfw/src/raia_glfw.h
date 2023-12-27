709a5462261b2a4961ef5f94e85bb67afc687c5b
dolphilia <dolphilia@outlook.com>
2023-12-28 03:06:14 +0900

    Update relative paths in CMakeLists and headers
    Relative paths in CMakeLists.txt and some header files were corrected to reflect the actual folder structure. This includes paths in src/static/static_callback.h, src/raia_glfw.h, and .idea/vcs.xml. This fix ensures the proper inclusion of required files during the build process.

--- src/binding/glfw/src/raia_glfw.h
+++ src/binding/glfw/src/raia_glfw.h
@@ -6,9 +6,9 @@
 #define RAIA_GUI_RAIA_GLFW_H
 
 #include <time.h>
-#include "../../../../common/c/utility/export_api.h"
-#include "../../../../common/c/utility/platform.h"
-#include "../../../../common/c/wrapper/wrapper_yyjson.h"
+#include "../../../common/c/utility/export_api.h"
+#include "../../../common/c/utility/platform.h"
+#include "../../../common/c/wrapper/wrapper_yyjson.h"
 #include "static/static_callback.h"
 #include "GLFW/glfw3.h" // Will drag system OpenGL headers
 
