a1c693f8e6cc8816150838f4014114c8323ec59e
dolphilia <dolphilia@outlook.com>
2023-12-28 12:39:20 +0900

    Update relative paths in includes
    The relative paths in include directives of several C files have been updated to correctly point to the necessary files. These changes were made in CMakeLists.txt, wrapper_stb_image.h, and raia_image.h files. This resolves potential build issues and dependency inconsistencies.

--- src/api/image/src/raia_image.h
+++ src/api/image/src/raia_image.h
@@ -2,9 +2,9 @@
 #define RAIA_IMAGE_RAIA_IMAGE_H
 
 #include <stdio.h>
-#include "../../../../common/c/utility/export_api.h"
-#include "../../../../common/c/utility/platform.h"
-#include "../../../../common/c/wrapper/wrapper_yyjson.h"
+#include "../../../common/c/utility/export_api.h"
+#include "../../../common/c/utility/platform.h"
+#include "../../../common/c/wrapper/wrapper_yyjson.h"
 #include "wrapper/wrapper_stb_image.h"
 
 #endif //RAIA_IMAGE_RAIA_IMAGE_H
