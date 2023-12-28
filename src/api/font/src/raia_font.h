a17b5e63edb9d57ec6acefcac6dffca74aaf7752
dolphilia <dolphilia@outlook.com>
2023-12-28 12:35:29 +0900

    Update relative paths in source files
    The modifications in this commit mainly concern the updating of relative paths in the "raia_font.h" and "CMakeLists.txt" files. These changes were necessary to correct the referencing of included files and dependencies, ensuring correctness of file inclusion and overall project organization.

--- src/api/font/src/raia_font.h
+++ src/api/font/src/raia_font.h
@@ -2,11 +2,11 @@
 #define RAIA_FONT_RAIA_FONT_H
 
 #include <stdio.h>
-#include "../../../../common/c/utility/export_api.h"
-#include "../../../../common/c/utility/platform.h"
-#include "../../../../common/c/wrapper/wrapper_yyjson.h"
+#include "../../../common/c/utility/export_api.h"
+#include "../../../common/c/utility/platform.h"
+#include "../../../common/c/wrapper/wrapper_yyjson.h"
 #define STB_TRUETYPE_IMPLEMENTATION
-#include "../../../../third_party/c/nothings/stb/stb_truetype.h"
+#include "../../../third_party/c/nothings/stb/stb_truetype.h"
 #include "util/util_pixel.h"
 #include "util/util_utf8.h"
 
