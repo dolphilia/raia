1064d9c9314b693f0c57c1f5d9193606ca1d029a
dolphilia <dolphilia@outlook.com>
2023-12-28 12:30:38 +0900

    Update relative file paths
    Paths to certain files in "src/raia_draw.h" and "CMakeLists.txt" were updated. The changes correct the file directory structure by reducing the depth of the relative paths, thus improving file location accuracy and process efficiency.

--- src/api/draw/src/raia_draw.h
+++ src/api/draw/src/raia_draw.h
@@ -3,9 +3,9 @@
 
 #include <stdio.h>
 #include <omp.h>
-#include "../../../../common/c/utility/export_api.h"
-#include "../../../../common/c/utility/platform.h"
-#include "../../../../common/c/wrapper/wrapper_yyjson.h"
+#include "../../../common/c/utility/export_api.h"
+#include "../../../common/c/utility/platform.h"
+#include "../../../common/c/wrapper/wrapper_yyjson.h"
 #include "util/util_math.h"
 #include "util/util_pixel.h"
 #include "util/util_utf8.h"
