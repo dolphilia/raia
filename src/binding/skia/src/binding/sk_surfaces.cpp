fc0084db3d7171435ea2a71dfb861cf1c50266fe
dolphilia <dolphilia@outlook.com>
2024-01-13 16:56:14 +0900

    Add SkSurfaces implementation and bindings
    This commit introduces the file sk_surfaces.cpp and sk_surfaces.h with the SkSurfaces implementation and its bindings. The bindings included are for the Null, Raster, and WrapPixels methods, with various overloads for each method. The implementation and bindings are intended for use with the Skia graphics library.

---
+++ src/binding/skia/src/binding/sk_surfaces.cpp
@@ -0,0 +1,33 @@
+//
+// Created by dolphilia on 2024/01/13.
+//
+
+#include "sk_surfaces.h"
+
+extern "C" {
+
+void SkSurfaces_Null(const char *sk_surface_key_out, int width, int height) {
+    static_sk_surface_set(sk_surface_key_out, SkSurfaces::Null(width, height));
+}
+
+void SkSurfaces_Raster(const char *sk_surface_key_out, const SkImageInfo *imageInfo, size_t rowBytes, const SkSurfaceProps *surfaceProps) {
+    static_sk_surface_set(sk_surface_key_out, SkSurfaces::Raster(*imageInfo, rowBytes, surfaceProps));
+}
+
+void SkSurfaces_Raster_2(const char *sk_surface_key_out, const SkImageInfo *imageInfo, const SkSurfaceProps *props) {
+    static_sk_surface_set(sk_surface_key_out, SkSurfaces::Raster(*imageInfo, props));
+}
+
+void SkSurfaces_WrapPixels(const char *sk_surface_key_out, const SkImageInfo *imageInfo, void *pixels, size_t rowBytes, const SkSurfaceProps *surfaceProps) {
+    static_sk_surface_set(sk_surface_key_out, SkSurfaces::WrapPixels(*imageInfo, pixels, rowBytes, surfaceProps));
+}
+
+void SkSurfaces_WrapPixels_2(const char *sk_surface_key_out, const SkPixmap *pm, const SkSurfaceProps *props) {
+    static_sk_surface_set(sk_surface_key_out, SkSurfaces::WrapPixels(*pm, props));
+}
+
+void SkSurfaces_WrapPixels_3(const char *sk_surface_key_out, const SkImageInfo *imageInfo, void *pixels, size_t rowBytes, SkSurfaces::PixelsReleaseProc pixelsReleaseProc, void *context, const SkSurfaceProps *surfaceProps) {
+    static_sk_surface_set(sk_surface_key_out, SkSurfaces::WrapPixels(*imageInfo, pixels, rowBytes, pixelsReleaseProc, context, surfaceProps));
+}
+
+}
---
+++ src/binding/skia/src/binding/sk_surfaces.h
@@ -0,0 +1,20 @@
+//
+// Created by dolphilia on 2024/01/13.
+//
+
+#ifndef RAIA_SKIA_SK_SURFACES_H
+#define RAIA_SKIA_SK_SURFACES_H
+
+#include "include/core/SkSurface.h"
+#include "../static/static_sk_surface.h"
+
+extern "C" {
+void SkSurfaces_Null(const char *sk_surface_key_out, int width, int height);
+void SkSurfaces_Raster(const char *sk_surface_key_out, const SkImageInfo *imageInfo, size_t rowBytes, const SkSurfaceProps *surfaceProps);
+void SkSurfaces_Raster_2(const char *sk_surface_key_out, const SkImageInfo *imageInfo, const SkSurfaceProps *props);
+void SkSurfaces_WrapPixels(const char *sk_surface_key_out, const SkImageInfo *imageInfo, void *pixels, size_t rowBytes, const SkSurfaceProps *surfaceProps);
+void SkSurfaces_WrapPixels_2(const char *sk_surface_key_out, const SkPixmap *pm, const SkSurfaceProps *props);
+void SkSurfaces_WrapPixels_3(const char *sk_surface_key_out, const SkImageInfo *imageInfo, void *pixels, size_t rowBytes, SkSurfaces::PixelsReleaseProc pixelsReleaseProc, void *context, const SkSurfaceProps *surfaceProps);
+}
+
+#endif //RAIA_SKIA_SK_SURFACES_H
