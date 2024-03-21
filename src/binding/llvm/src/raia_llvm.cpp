e8f95322d098c74d462c03fee88956eb779e47ae
dolphilia <dolphilia@outlook.com>
2024-03-22 03:37:30 +0900

    Add .idea configurations and create new `raia_llvm.cpp` file
    The update includes the creation of `.idea` configuration files for the project and a new cpp file `raia_llvm.cpp`. The `raia_llvm.cpp` file contains initial code set up, including the structure for a main function and various includes for the LLVM library. Furthermore, the `.idea` files contain details about the project's modules and vcs mapping.

---
+++ src/binding/llvm/src/raia_llvm.cpp
@@ -0,0 +1,86 @@
+#include <llvm/IR/BasicBlock.h>
+#include <llvm/IR/Function.h>
+#include <llvm/IR/IRBuilder.h>
+#include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/Module.h>
+#include <llvm/ExecutionEngine/ExecutionEngine.h>
+#include <llvm/ExecutionEngine/MCJIT.h>
+#include <llvm/ExecutionEngine/GenericValue.h> // この行を追加
+#include <llvm/Support/TargetSelect.h>
+#include <llvm/Support/InitLLVM.h>
+#include <llvm/Support/raw_ostream.h>
+#include <memory>
+
+using namespace llvm;
+
+int main() {
+    InitializeNativeTarget();
+    InitializeNativeTargetAsmPrinter();
+    InitializeNativeTargetAsmParser();
+
+    LLVMContext context;
+    IRBuilder<> builder(context);
+    std::unique_ptr<Module> module = std::make_unique<Module>("my_module", context);
+
+    FunctionType *funcType = FunctionType::get(builder.getInt32Ty(), {builder.getInt32Ty(), builder.getInt32Ty(), builder.getInt32Ty()}, false);
+    Function *function = Function::Create(funcType, Function::ExternalLinkage, "f", module.get());
+
+    auto args = function->arg_begin();
+    Value *x = args++;
+    x->setName("x");
+    Value *y = args++;
+    y->setName("y");
+    Value *z = args++;
+    z->setName("z");
+
+    BasicBlock *entry = BasicBlock::Create(context, "entry", function);
+    builder.SetInsertPoint(entry);
+
+    Value *counter = builder.CreateAlloca(builder.getInt32Ty(), nullptr, "counter");
+    Value *result = builder.CreateAlloca(builder.getInt32Ty(), nullptr, "result");
+    builder.CreateStore(x, result);
+    builder.CreateStore(builder.getInt32(0), counter);
+
+    BasicBlock *loopStart = BasicBlock::Create(context, "loop.start", function);
+    builder.CreateBr(loopStart);
+
+    builder.SetInsertPoint(loopStart);
+    Value *currentCounter = builder.CreateLoad(builder.getInt32Ty(), counter, "current.counter");
+    Value *currentResult = builder.CreateLoad(builder.getInt32Ty(), result, "current.result");
+
+    BasicBlock *loopEnd = BasicBlock::Create(context, "loop.end", function);
+    BasicBlock *afterLoop = BasicBlock::Create(context, "after.loop", function);
+    Value *endCondition = builder.CreateICmpEQ(currentCounter, z, "end.condition");
+    builder.CreateCondBr(endCondition, afterLoop, loopEnd);
+
+    builder.SetInsertPoint(loopEnd);
+    Value *updatedResult = builder.CreateAdd(currentResult, y, "updated.result");
+    Value *updatedCounter = builder.CreateAdd(currentCounter, builder.getInt32(1), "updated.counter");
+    builder.CreateStore(updatedResult, result);
+    builder.CreateStore(updatedCounter, counter);
+    builder.CreateBr(loopStart);
+
+    builder.SetInsertPoint(afterLoop);
+    Value *finalResult = builder.CreateLoad(builder.getInt32Ty(), result, "final.result");
+    builder.CreateRet(finalResult);
+
+    std::string errStr;
+    ExecutionEngine *ee = EngineBuilder(std::move(module))
+            .setErrorStr(&errStr)
+            .create();
+    if (!ee) {
+        errs() << "Failed to create ExecutionEngine: " << errStr << "\n";
+        return 1;
+    }
+
+    // 実行エンジンを使用して関数を実行
+    std::vector<GenericValue> argValues(3);
+    argValues[0].IntVal = APInt(32, 0); // x = 0
+    argValues[1].IntVal = APInt(32, 1); // y = 1
+    argValues[2].IntVal = APInt(32, 10); // z = 10
+
+    GenericValue resultValue = ee->runFunction(function, argValues);
+    outs() << "Result of f(0, 1, 10): " << resultValue.IntVal << "\n";
+
+    return 0;
+}
