28e21191e0d809560814aee5bac33e3531980d8a
dolphilia <dolphilia@outlook.com>
2024-07-17 12:51:34 +0900

    Extend ImGui wrapper with additional functions
    The ImGui wrapper has been significantly expanded with a multitude of additional functions. These additions include new window operation and parameter stack methods, extended controls for the cursor and layout, more options for widget displays, as well as added identifiers and input handling.

--- src/binding/imgui/src/raia_imgui.h
+++ src/binding/imgui/src/raia_imgui.h
@@ -95,53 +95,156 @@ float raia_imgui_get_window_width();
 float raia_imgui_get_window_height();
 
 // ウィンドウ操作
-void raia_imgui_set_next_window_pos(float pos_x, float pos_y, ImGuiCond cond = 0, float pivot_x = 0, float pivot_y = 0);
-void raia_imgui_set_next_window_viewport(ImGuiViewport* viewport);
+void raia_imgui_set_next_window_pos(float pos_x, float pos_y, ImGuiCond cond /* = 0 */, float pivot_x /* = 0 */, float pivot_y /* = 0 */);
+void raia_imgui_set_next_window_size(float size_x, float size_y, ImGuiCond cond /* = 0 */);
+void raia_imgui_set_next_window_size_constraints(float size_min_x, float size_min_y, float size_max_x, float size_max_y, ImGuiSizeCallback custom_callback /* = NULL*/, void* custom_callback_data /* = NULL*/);
+void raia_imgui_set_next_window_content_size(float size_x, float size_y);
+void raia_imgui_set_next_window_collapsed(bool collapsed, ImGuiCond cond /*= 0*/);
+void raia_imgui_set_next_window_focus();
+void raia_imgui_set_next_window_scroll(float scroll_x, float scroll_y);
 void raia_imgui_set_next_window_bg_alpha(float alpha);
+void raia_imgui_set_next_window_viewport(ImGuiViewport* viewport);
+void raia_imgui_set_next_window_viewport_2(ImGuiID viewport_id);
+void raia_imgui_set_window_pos(float pos_x, float pos_y, ImGuiCond cond /* = 0*/);
+void raia_imgui_set_window_size(float size_x, float size_y, ImGuiCond cond /* = 0 */);
+void raia_imgui_set_window_collapsed(bool collapsed, ImGuiCond cond /* = 0*/);
+void raia_imgui_set_window_focus();
+void raia_imgui_set_window_font_scale(float scale);
+void raia_imgui_set_window_pos_2(const char* name, float pos_x, float pos_y, ImGuiCond cond /*= 0*/);
+void raia_imgui_set_window_size_2(const char* name, float size_x, float size_y, ImGuiCond cond /*= 0*/);
+void raia_imgui_set_window_collapsed_2(const char* name, bool collapsed, ImGuiCond cond /*= 0*/);
+void raia_imgui_set_window_focus_2(const char* name);
 
 // コンテンツ領域
 
+float raia_imgui_get_content_region_avail_x();
+float raia_imgui_get_content_region_avail_y();
+float raia_imgui_get_content_region_max_x();
+float raia_imgui_get_content_region_max_y();
+float raia_imgui_get_window_content_region_min_x();
+float raia_imgui_get_window_content_region_min_y();
+float raia_imgui_get_window_content_region_max_x();
+float raia_imgui_get_window_content_region_max_y();
+
 // ウィンドウ・スクロール
+float raia_imgui_get_scroll_x();
+float raia_imgui_get_scroll_y();
+void raia_imgui_set_scroll_x(float scroll_x);
+void raia_imgui_set_scroll_y(float scroll_y);
+float raia_imgui_get_scroll_max_x();
+float raia_imgui_get_scroll_max_y();
+void raia_imgui_set_scroll_here_x(float center_x_ratio /* = 0.5f*/);
+void raia_imgui_set_scroll_here_y(float center_y_ratio /* = 0.5f*/);
+void raia_imgui_set_scroll_from_pos_x(float local_x, float center_x_ratio /* = 0.5f*/);
+void raia_imgui_set_scroll_from_pos_y(float local_y, float center_y_ratio /* = 0.5f*/);
 
 // パラメータ・スタック（共有）
-void raia_imgui_push_style_color_u32(ImGuiCol idx, ImU32 col);
-void raia_imgui_push_style_color_vec4(ImGuiCol idx, float x, float y, float z, float w);
-void raia_imgui_pop_style_color(int count);
-void raia_imgui_push_style_var_float(ImGuiStyleVar idx, float val);
-void raia_imgui_push_style_var_vec2(ImGuiStyleVar idx, float x, float y);
-void raia_imgui_pop_style_var(int count);
+void raia_imgui_push_font(ImFont* font);
+void raia_imgui_pop_font();
+void raia_imgui_push_style_color(ImGuiCol idx, ImU32 col);
+void raia_imgui_push_style_color_2(ImGuiCol idx, float col_x, float col_y, float col_z, float col_w);
+void raia_imgui_pop_style_color_3(int count /* = 1*/);
+void raia_imgui_push_style_var(ImGuiStyleVar idx, float val);
+void raia_imgui_push_style_var_2(ImGuiStyleVar idx, float val_x, float val_y);
+void raia_imgui_pop_style_var_3(int count /* = 1*/);
+void raia_imgui_push_tab_stop(bool tab_stop);
+void raia_imgui_pop_tab_stop();
+void raia_imgui_push_button_repeat(bool repeat);
+void raia_imgui_pop_button_repeat();
 
 // パラメータ・スタック（現在のウィンドウ）
+void raia_imgui_push_item_width(float item_width);
+void raia_imgui_pop_item_width();
+void raia_imgui_set_next_item_width(float item_width);
+float raia_imgui_calc_item_width();
+void raia_imgui_push_text_wrap_pos(float wrap_local_pos_x /* = 0.0f*/);
+void raia_imgui_pop_text_wrap_pos();
 
 // スタイルの読み込みとアクセス
+ImFont* raia_imgui_get_font();
+float raia_imgui_get_font_size();
+float raia_imgui_get_font_tex_uv_white_pixel_x();
+float raia_imgui_get_font_tex_uv_white_pixel_y();
+ImU32 raia_imgui_get_color_u32(ImGuiCol idx, float alpha_mul /* = 1.0f*/);
+ImU32 raia_imgui_get_color_u32_2(float col_x, float col_y, float col_z, float col_w);
+ImU32 raia_imgui_get_color_u32_3(ImU32 col);
+float raia_imgui_get_style_color_vec4_x(ImGuiCol idx);
+float raia_imgui_get_style_color_vec4_y(ImGuiCol idx);
+float raia_imgui_get_style_color_vec4_z(ImGuiCol idx);
+float raia_imgui_get_style_color_vec4_w(ImGuiCol idx);
 
 // カーソル / レイアウト
 void raia_imgui_separator();
 void raia_imgui_same_line();
+void raia_imgui_new_line();
+void raia_imgui_spacing();
+void raia_imgui_dummy(float size_x, float size_y);
+void raia_imgui_indent(float indent_w /* = 0.0f*/);
+void raia_imgui_unindent(float indent_w /* = 0.0f*/);
+void raia_imgui_begin_group();
+void raia_imgui_end_group();
+//ImVec2 raia_imgui_GetCursorPos();
+float raia_imgui_get_cursor_pos_x();
+float raia_imgui_get_cursor_pos_y();
+void raia_imgui_set_cursor_pos(float local_pos_x, float local_pos_y);
+void raia_imgui_set_cursor_pos_x(float local_x);
+void raia_imgui_set_cursor_pos_y(float local_y);
+float raia_imgui_get_cursor_start_pos_x();
+float raia_imgui_get_cursor_start_pos_y();
+float raia_imgui_get_cursor_screen_pos_x();
+float raia_imgui_get_cursor_screen_pos_y();
+void raia_imgui_set_cursor_screen_pos(float pos_x, float pos_y);
+void raia_imgui_align_text_to_frame_padding();
+float raia_imgui_get_text_line_height();
+float raia_imgui_get_text_line_height_with_spacing();
+float raia_imgui_get_frame_height();
+float raia_imgui_get_frame_height_with_spacing();
 
 // IDスタック / スコープ
+void raia_imgui_push_id(const char* str_id);
+void raia_imgui_push_id_2(const char* str_id_begin, const char* str_id_end);
+void raia_imgui_push_id_3(const void* ptr_id);
+void raia_imgui_push_id_4(int int_id);
+void raia_imgui_pop_id();
+ImGuiID raia_imgui_get_id(const char* str_id);
+ImGuiID raia_imgui_get_id_2(const char* str_id_begin, const char* str_id_end);
+ImGuiID raia_imgui_get_id_3(const void* ptr_id);
 
 // ウィジェット: テキスト
 void raia_imgui_text(const char *text);
 
 // ウィジェット: メイン
 bool raia_imgui_button(const char *label, float width, float height);
+bool raia_imgui_small_button(const char* label);
+bool raia_imgui_invisible_button(const char* str_id, float size_x, float size_y, ImGuiButtonFlags flags /* = 0*/);
+bool raia_imgui_arrow_button(const char* str_id, ImGuiDir dir);
 bool raia_imgui_checkbox(const char *label, bool *v);
+bool raia_imgui_checkbox_flags(const char* label, int* flags, int flags_value);
+bool raia_imgui_checkbox_flags_2(const char* label, unsigned int* flags, unsigned int flags_value);
+bool raia_imgui_radio_button(const char* label, bool active);
+bool raia_imgui_radio_button_2(const char* label, int* v, int v_button);
+void raia_imgui_progress_bar(float fraction, float size_arg_x /*= -FLT_MIN*/, float size_arg_y /*= 0*/, const char* overlay /* = NULL*/);
+void raia_imgui_bullet();
 
 // ウィジェット: 画像
 void raia_imgui_image(void *texture_id, float width, float height);
+bool raia_imgui_image_button(const char* str_id, ImTextureID user_texture_id, float size_x, float size_y,
+                             float uv0_x /* = 0 */, float uv0_y /* = 0 */,
+                             float uv1_x /* = 1 */, float uv1_y /* = 1 */,
+                             float bg_col_x /* = 0 */, float bg_col_y /* = 0 */, float bg_col_z /* = 0 */, float bg_col_w /* = 0 */,
+                             float tint_col_x /* = 1 */, float tint_col_y /* = 1 */, float tint_col_z /* = 1 */, float tint_col_w /* = 1 */);
 
 // ウィジェット: コンボボックス（ドロップダウン）
 
 // ウィジェット: ドラッグスライダー
 
 // ウィジェット: レギュラー・スライダー
-float raia_imgui_slider_float(const char *label, float *v, float v_min, float v_max);
+float raia_imgui_slider_float(const char *label, float *v, float v_min, float v_max, const char* format /* = "%.3f" */, ImGuiSliderFlags flags /* = 0 */);
 
 // ウィジェット: キーボード入力
 
 // ウィジェット: カラーエディター/ピッカー
-bool raia_imgui_color_edit_3(const char *label, float *col);
+bool raia_imgui_color_edit_3(const char *label, float *col, ImGuiColorEditFlags flags /* = 0 */);
 
 // ウィジェット: ツリー
 
